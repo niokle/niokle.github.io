aa2344e74a4502eab866ae339062eb7de29e08b3
Michał Kleszczewski <michalkleszczewski@macbook-pro-micha.home>
2019-03-24 14:22:12 +0100

    task 23.1 commit 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://ancient-bastion-65075.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://ancient-bastion-65075.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://secret-lowlands-80376.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://secret-lowlands-80376.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
